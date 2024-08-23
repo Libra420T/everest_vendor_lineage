deb3b18a7acf46a3e79ddca574657dbf9b84c3e5
Libra420T <ryantf420@gmail.com>
2024-08-23 17:12:03 -0400

    Config: Adjust sensitive_pn.xml location call

--- config/telephony.mk
+++ config/telephony.mk
@@ -5,7 +5,7 @@ endif
 
 # Sensitive Phone Numbers list
 PRODUCT_PACKAGES += \
-    sensitive_pn.xml
+    vendor/lineage/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
 # World APN list
 PRODUCT_PACKAGES += \
