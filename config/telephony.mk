6165c49906f757e7b28b300bc7c81acc4a1800f0
Libra420T <ryantf420@gmail.com>
2024-08-23 15:56:25 -0400

    Telephony: Add & Include Pixel APNs

--- config/telephony.mk
+++ config/telephony.mk
@@ -303,3 +303,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Include Carrier Runtime Configuration
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/lineage/CarrierSettings/overlay
+    
+include vendor/everest/config/pixel_telephony.mk
+
